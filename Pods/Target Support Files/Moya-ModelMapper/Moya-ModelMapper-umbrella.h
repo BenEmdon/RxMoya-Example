8c5aecc100bc198206321faa670f1e73d272acfb
BenEmdon <benjaminemdon@gmail.com>
2017-01-16 14:26:32 -0500

    Removed everything to make space for a stable rebuild

--- Pods/Target Support Files/Moya-ModelMapper/Moya-ModelMapper-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Moya_ModelMapperVersionNumber;
-FOUNDATION_EXPORT const unsigned char Moya_ModelMapperVersionString[];
-
--- Pods/Target Support Files/Pods-RxMoyaExample/Pods-RxMoyaExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxMoyaExample : NSObject
-@end
-@implementation PodsDummy_Pods_RxMoyaExample
-@end
